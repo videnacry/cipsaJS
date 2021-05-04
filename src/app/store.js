76456a3a628163487ccc73ec3db7fdd6b39dc5da
beron <bleink@outlook.es>
2021-05-04 13:47:01 +0200

    change store by default

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import {configureStore} from '@reduxjs/toolkit'
+import directory from '../features/directory/slice'
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+    reducer: {
+        directory
+    }
+})
