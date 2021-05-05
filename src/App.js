986ba12f5175e0150626ee80e9aa3b48193d98c4
beron <bleink@outlook.es>
2021-05-05 12:38:36 +0200

    change structure in App for nav and main responsive

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {useSelector} from 'react-redux'
 import {selectRoot} from './features/directory/slice'
-import {Container, Row} from 'react-bootstrap'
+import {Container, Row, Col} from 'react-bootstrap'
 import Annex from './annex'
 import Nav from './features/directory'
 
@@ -10,13 +10,17 @@ function App() {
   const root = useSelector(selectRoot)
   console.log(root)
   return (
-    <Row className="p-4 bg-dark bg-gradient vh-100">
-      <section>
+    <Row className="p-4 bg-dark bg-gradient vh-100 overflow-auto">
+      <Col xs="3" sm="2" lg="3">
+      </Col>
+      <Col xs="9" sm="9" lg="7">
+        <Container>
+          {roots[root]}
+        </Container>
+      </Col>
+      <section className="position-fixed">
         <Nav/>
       </section>
-      <Container>
-        {roots[root]}
-      </Container>
     </Row>
   );
 }
