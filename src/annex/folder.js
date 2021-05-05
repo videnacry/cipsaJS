c6a20c50ac682208790c485de4ba123d14c17732
beron <bleink@outlook.es>
2021-05-05 12:36:54 +0200

    add folder component to show when a folder opened instead of a file

---
+++ src/annex/folder.js
@@ -0,0 +1,27 @@
+import {memo} from 'react'
+import {Row, Col, Button} from 'react-bootstrap'
+import folderSVG from './folder.svg'
+
+const render = files => {
+    return Object.values(files.children).map(file => 
+        // <Col xs="6" sm="4" md="3">
+            <Button variant="outline-primary">
+                <img src={folderSVG} alt="folder img" width="100px"/>
+                <h6>{file.name}</h6>
+            </Button>
+        // </Col>
+    )
+}
+const Folder = memo(({files}) => {
+
+    return(
+        <Row className="justify-content-center">
+            {render(files)}
+            <Col xs="12" className="mt-2">
+                <div className="d-flex justify-content-center">Icons made by <a href="https://www.flaticon.com/authors/dinosoftlabs" title="DinosoftLabs">DinosoftLabs</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+            </Col>
+        </Row>
+    )
+})
+
+export default Folder
